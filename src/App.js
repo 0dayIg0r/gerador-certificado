60d061984e11de41d9293ff9f5d6d5fd1b3e0cdc
unknown <igormedeiros.f@gmail.com>
2023-03-20 10:29:24 -0300

    upando

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import Certificate from "./components/Certificate";
+
+import { useState } from "react";
+
 
 function App() {
+  const [name, setName] = useState('Maisa Silva Sousa')
+  const [teacherName, setTeacherName] = useState('Igor Medeiros Freitas')
+  const [h, setH] = useState(20)
+  const [courseName, setCourseName] = useState('Pacote Office')
+  const [date, setDate] = useState('16/10/2022')
+  const [endDate, setEndDate] = useState('20/10/2022')
+  const [directorName, setDirectorName] = useState('Nome do diretor(a)')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Certificate
+        name={name}
+        teacherName={teacherName}
+        h={h}
+        courseName={courseName}
+        date={date}
+        endDate={endDate}
+
+      />
     </div>
   );
 }
---
+++ src/components/Certificate.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import Medal from '../assets/medal.png'
+import Asp from '../assets/asp-logo.png'
+import styles from './styles.module.scss'
+
+
+
+const Certificate = ({ name, teacherName, h, courseName, date, endDate }) => {
+  return (
+    <div className={styles.container}>
+
+      <div className={styles.content}>
+        <span>
+          <h1>Certificado de Conclusão</h1>
+        </span>
+
+        <div className={styles.subTitle}>
+          <span>Ação Social do Planalto concede a</span>
+        </div>
+        <div className={styles.name}>
+          <h1>{name}</h1>
+        </div>
+        <div className={styles.infos}>
+          <span>
+            O certificado de conclusão do curso de {courseName},
+            ministrado pelo profissional {teacherName},
+            entre o período de {date} e {endDate}
+          </span>
+        </div>
+      </div>
+      <div className={styles.signatureContainer}>
+        <div className={styles.signature}>
+          <span>________________________________________</span>          </div>
+        <div className={styles.superiorSignature}>
+          <span>Assinatura do diretor(a)</span>
+        </div>
+
+        <div className={styles.signature}>
+          <span>________________________________________</span>
+        </div>
+        <div className={styles.studentSignature}>
+          <span>Assinatura do aluno(a)</span>
+        </div>
+        <div className={styles.logo}>
+          <img src={Medal} />
+          <img src={Asp} />
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Certificate
